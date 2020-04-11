77e6745349541bf8dd20c63811abcbbb8dc0de49
danielbs93 <danielbs093@gmail.com>
2020-04-11 20:57:50 +0300

    Main in its way to be finished

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class Main {
 
-    //Flags for encryption/decryption//
+    // Flags for encryption/decryption //
     private static final String Encrypt_Instruction = "-e";
     private static final String Decrypt_Instruction = "-d";
     private static final String Keys_flag = "-k";
     private static final String Input_flag = "-i";
-    private static final String Output_flag = "-o";
+    private static final String Output_flag = "-o";//use for breaking too
 
-    //Flags for breaking//
+    // Flags for breaking //
     private static final String Break_Instruction = "-b";
     private static final String PlainTextPathDenotes = "-m";
     private static final String CypherTextPathDenotes = "-c";
-    private static final String FoundKeysPathDenotes = "-o";
 
-    //Paths
+    // Paths //
     private static String Key_Path; // path of the keys to encrypt/decrypte or for output breaking keys file
     private static String InputMassagePath; //path to a file we want to encrypt/decrypt or the plain-text to break
     private static String OutputMassagePath;// path to the output file or the cypher-text we want to break
 
+    // Fields //
+    private static Byte[][] Key1 = new Byte[4][4];
+    private static Byte[][] Key2 = new Byte[4][4];
+    private static Byte[][] Key3 = new Byte[4][4];
+    private static ArrayList<Byte[][]> PlainText = new ArrayList<>();
+    private static ArrayList<Byte[][]> CypherText = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
@@ -30,20 +34,31 @@ public class Main {
                 Key_Path = args[2];
                 InputMassagePath = args[4];
                 OutputMassagePath = args[6];
-                readData(Key_Path,InputMassagePath,OutputMassagePath);
+                createByteKeys(Key_Path);
                 //Encryption
                 if (args[0].equals(Encrypt_Instruction)) {
-                    Encryptor encryptor = new Encryptor();
+                    readData(InputMassagePath,PlainText);
+                    Encryptor encryptor = new Encryptor();// TODO: passing parameters
+                    //TODO: encrypt method + saving to OutputMassagePath
                 } else
                 //Decryption
                 if (args[0].equals(Decrypt_Instruction)) {
-                    Decryptor decryptor = new Decryptor();
+                    readData(OutputMassagePath, CypherText);
+                    Decryptor decryptor = new Decryptor();// TODO: passing parameters
+                    //TODO: decrypt method + saving to InputMassagePath
                 }
             }
             else
             //Breaking
-            if (args[0].equals(Break_Instruction)) {
-                Breaker breaker = new Breaker();
+            if (args[0].equals(Break_Instruction) && args[1].equals(PlainTextPathDenotes)
+                && args[3].equals(CypherTextPathDenotes) && args[5].equals(Output_flag)) {
+                InputMassagePath = args[2];
+                OutputMassagePath = args[4];
+                Key_Path = args[6];
+                readData(InputMassagePath,PlainText);
+                readData(OutputMassagePath,CypherText);
+                Breaker breaker = new Breaker();// TODO: passing parameters
+                //TODO: break method + saving to Key_path
             }
             else //illegal input
                 throw new Exception();
@@ -53,14 +68,71 @@ public class Main {
 
     }
 
-    private static void readData(String key_path, String inputMassagePath, String outputMassagePath) {
-        File input = new File(inputMassagePath);
-        File output = new File(outputMassagePath);
+    /**
+     * Creating the 3 keys according to the key_path.
+     * Each key is in size of 128bit = 16 bytes
+     * Storing them into the private fields Key1, Key2, Key3 mentioned in the beginning of this code section
+     * @param key_path
+     */
+    private static void createByteKeys(String key_path) {
         File keys = new File(key_path);
         try {
-            FileReader myReader = new FileReader(input);
+            byte[] massage = Files.readAllBytes(keys.toPath());
+            byte[] key1 = new byte[16];
+            byte[] key2 = new byte[16];
+            byte[] key3 = new byte[16];
+            for (int i = 0; i < 48; i++) {
+                if (i < 16){
+                    key1[i] = massage[i];
+                }else
+                    if (i > 15 && i < 32) {
+                        key2[i] = massage[i];
+                    }else
+                        if (i > 31) {
+                            key3[i] = massage[i];
+                        }
+            }
+            buildMatrixData(key1,Key1);
+            buildMatrixData(key2,Key2);
+            buildMatrixData(key3,Key3);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        } catch (FileNotFoundException e) {
+    }
+
+    /**
+     *
+     * @param arrayData one byte array contains all 16 bytes of the key
+     * @param matrixData 4x4 matrix that contains in each cell 1 byte of data
+     * The composition of each key is taking each byte from key3 and put it in a column ordered data
+     */
+    private static void buildMatrixData(byte[] arrayData, Byte[][] matrixData) {
+        int index = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrixData[j][i] = arrayData[index];
+                index++;
+            }
+        }
+    }
+
+    private static void readData(String MassagePath, ArrayList<Byte[][]> plainText) {
+        File path = new File(MassagePath);
+        try {
+            byte[] massage = Files.readAllBytes(path.toPath());
+            int numOfBlocks = massage.length/16;
+            int index = 0;
+            for (int i = 0; i < numOfBlocks; i++) {
+                plainText.set(i,new Byte[4][4]);
+                byte[] block = new byte[16];
+                for (int j = 0; j < 16; j++) {
+                    block[j] = massage[j+index];
+                }
+                buildMatrixData(block,plainText.get(i));
+                index += 16;
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
